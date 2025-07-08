74295aeddb273aa0999b636f7ace40dcfe4e1d8f
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-08 14:10:36 +0900

    add: AesConfig 추가

---
+++ src/main/java/com/han/oauth_practice/security/AesConfig.java
@@ -0,0 +1,24 @@
+package com.han.oauth_practice.security;
+
+import com.han.oauth_practice.utils.aes.AesUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+public class AesConfig {
+    @Value("${data.aes-cipher.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public SecretKeySpec secretKeySpec() {
+        return new SecretKeySpec(secretKey.getBytes(), "AES");
+    }
+
+    @Bean
+    public AesUtil aesUtil(SecretKeySpec secretKeySpec) {
+        return new AesUtil(secretKeySpec);
+    }
+}

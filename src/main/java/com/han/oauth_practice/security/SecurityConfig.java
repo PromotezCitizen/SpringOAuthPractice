df09cf21a47ab0f6db7ca5320e875e578b6aa815
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-10 14:16:55 +0900

    add: security filter 추가
    - 기본 로그인 이전 custom login filter를 적용하여 로그인 진행

--- src/main/java/com/han/oauth_practice/security/SecurityConfig.java
+++ src/main/java/com/han/oauth_practice/security/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.han.oauth_practice.security;
 
+import com.han.oauth_practice.member.repository.MemberRepository;
+import com.han.oauth_practice.security.filter.CustomUsernamePasswordAuthenticationFilter;
 import com.han.oauth_practice.security.handler.CustomOAuth2FailureHandler;
 import com.han.oauth_practice.security.handler.CustomOAuth2SuccessHandler;
 import com.han.oauth_practice.security.resolver.CustomOAuth2AuthorizationRequestResolver;
 import com.han.oauth_practice.security.service.CustomOAuth2UserService;
+import com.han.oauth_practice.utils.aes.AesUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,10 +25,18 @@ public class SecurityConfig {
     private final CustomOAuth2SuccessHandler oauth2SuccessHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   CustomUsernamePasswordAuthenticationFilter customUsernamePasswordAuthenticationFilter
+    ) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error=true")
+                        .permitAll()
                 )
                 .oauth2Login(oauth -> oauth
                         .loginPage("/login") // <-- 인증이 필요한 경로로 들어갔을 때 리다이렉트 하는 경로
@@ -40,6 +52,13 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/") // <-- 로그아웃 후 리다이렉트 하는 경로
                 );
 
+        http.addFilterBefore(customUsernamePasswordAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public CustomUsernamePasswordAuthenticationFilter customUsernamePasswordAuthenticationFilter(AesUtil aesUtil, MemberRepository memberRepository) {
+        return new CustomUsernamePasswordAuthenticationFilter(aesUtil, memberRepository);
     }
 }

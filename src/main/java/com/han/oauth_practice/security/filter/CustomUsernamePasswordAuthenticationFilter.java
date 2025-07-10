8d4053d0faeb78febe58cb07714bfde63b28f59c
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-10 14:12:12 +0900

    add: 로그인 필터 추가
    - id/pw 로그인 시 UsernameFilter 이전에 사용자 인증 진행

---
+++ src/main/java/com/han/oauth_practice/security/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.han.oauth_practice.security.filter;
+
+import com.han.oauth_practice.member.repository.MemberRepository;
+import com.han.oauth_practice.member.entity.Member;
+import com.han.oauth_practice.utils.aes.AesUtil;
+import com.han.oauth_practice.utils.cookie.CookieUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class CustomUsernamePasswordAuthenticationFilter extends OncePerRequestFilter {
+    private final AesUtil aesUtil;
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if ("/login".equals(request.getRequestURI()) && HttpMethod.POST.toString().equals(request.getMethod())) {
+
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            Optional<Member> memberOpt = memberRepository.findByUsername(username);
+            if (memberOpt.isEmpty()) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            Member member = memberOpt.get();
+            String encryptedPassword = member.getPassword();
+
+            String decryptedPassword;
+            try {
+                decryptedPassword = aesUtil.decrypt(encryptedPassword);
+                if (!decryptedPassword.equals(password)) return;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            UserDetails userDetails = User.builder()
+                    .username(username)
+                    .password(decryptedPassword)
+                    .roles(String.valueOf(member.getRole()))
+                    .build();
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            CookieUtil.add(response, "X-User-Id", member.getId().toString());
+            response.sendRedirect("/");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}

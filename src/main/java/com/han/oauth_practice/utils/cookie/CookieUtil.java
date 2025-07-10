fc4544a66d9d88e3c36a16861854e232630d3eef
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-10 14:10:53 +0900

    add: 공통 cookie util 추가

---
+++ src/main/java/com/han/oauth_practice/utils/cookie/CookieUtil.java
@@ -0,0 +1,61 @@
+package com.han.oauth_practice.utils.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+    public static void delete(HttpServletResponse response,
+                              String name,
+                              boolean isHttpOnly,
+                              boolean isSecure) {
+        Cookie cookie = createCookie(name, "", 0, isHttpOnly, isSecure);
+        response.addCookie(cookie);
+    }
+
+    public static void delete(HttpServletResponse response, String name) {
+        delete(response, name, false, false);
+    }
+
+    public static void add(HttpServletResponse response,
+                           String name,
+                           String value,
+                           int maxAge,
+                           boolean isHttpOnly,
+                           boolean isSecure) {
+        Cookie cookie = createCookie(name, value, maxAge, isHttpOnly, isSecure);
+        response.addCookie(cookie);
+    }
+
+    public static void add(HttpServletResponse response,
+                           String name,
+                           String value,
+                           boolean isHttpOnly,
+                           boolean isSecure) {
+        add(response, name, value, -1, isHttpOnly, isSecure);
+    }
+
+    public static void add(HttpServletResponse response,
+                           String name,
+                           String value,
+                           int maxAge) {
+        add(response, name, value, maxAge, false, false);
+    }
+
+    public static void add(HttpServletResponse response, String name, String value) {
+        add(response, name, value, -1,false, false);
+    }
+
+    private static Cookie createCookie(String name,
+                                       String value,
+                                       int maxAge,
+                                       boolean isHttpOnly,
+                                       boolean isSecure) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setDomain("localhost");
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        cookie.setHttpOnly(isHttpOnly);
+        cookie.setSecure(isSecure);
+        return cookie;
+    }
+}

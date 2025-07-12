f875d7c797c2b8933b951d3571782e980241bb75
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-12 14:19:19 +0900

    annotation: 함수 반환값 캐싱 기능 추가

--- src/main/java/com/han/oauth_practice/OauthPracticeApplication.java
+++ src/main/java/com/han/oauth_practice/OauthPracticeApplication.java
@@ -2,8 +2,10 @@ package com.han.oauth_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OauthPracticeApplication {
 
 	public static void main(String[] args) {
